57374b2e5b6623a4a1f7ee5bffee2a97766b7415
randyarbolaez <randyarbolaez@outlook.com>
2021-06-11 09:45:55 -0400

    added documentation

--- util/sentiment-analysis.js
+++ util/sentiment-analysis.js
@@ -1,18 +1,29 @@
-const Words = require('./words-and-their-value.js');
+const Words = require("./words-and-their-value.js");
 
-const formatString = (str) => (str.match(/[^_\W^\d]+/g).join(' ').toLowerCase().split(' '));
+/*
+  ^ = opposite
+  _ = match underscores
+  \W = match !alphanumeric, also doesnt match underscores
+  0-9 = match only numbers
+  + = match one or more characters (no empty strings)
+*/
+
+const formatString = (str) =>
+  str
+    .match(/[^_\W0-9]+/g)
+    .join(" ")
+    .toLowerCase()
+    .split(" ");
 
 const sentimentAnalysis = (str) => {
   let score = 0;
   let formattedString = formatString(str);
-  for(let i = 0;i< formattedString.length;i++){
-
-    if(!Words[formattedString[i]]){
+  for (let i = 0; i < formattedString.length; i++) {
+    if (!Words[formattedString[i]]) {
       score += 0;
-    }else{
+    } else {
       score += Words[formattedString[i]];
     }
-
   }
   return score;
 };
